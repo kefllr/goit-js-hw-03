dc467c110a09c13e00a48037518034e3b1452db9
kefllr <timislusar05@gmail.com>
2023-11-28 02:12:00 +0100

    goit-js-hw-03

---
+++ js/task-2.js
@@ -0,0 +1,13 @@
+function makeArray(firstArray, secondArray, maxLength){
+    const arrayConcat = firstArray.concat(secondArray);
+    const arraySlice = arrayConcat.slice(0, maxLength);
+    if (arrayConcat.length >= maxLength) {
+        return arraySlice;
+    };
+}
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
